8a909faf917c9124fd91287d7b5cf4d593903473
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-12 21:03:04 -0300

    Increase contrast for disabled buttons

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -37,7 +37,7 @@ export default function Wallet({
 						Wallet Connected
 					</div>
 					<div
-						class={classNames(
+						className={classNames(
 							"network",
 							isRinkeby ? "networkValid" : "networkInvalid",
 						)}
